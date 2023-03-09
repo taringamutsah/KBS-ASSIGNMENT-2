8c274a396c0b1398c955f3cf092ab9d5640c8ada
Mutsa Taringa <mutsahfx@gmail.com>
2023-03-09 18:17:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,28 @@ from flask_migrate import Migrate
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+from wtforms.validators import InputRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mykey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+# validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!'), FileSize(max_size=5000000)]
 
 class UploadFileForm(FlaskForm):
-    file = FileField('File')
+    file = FileField('File',  validators=[InputRequired()])
+    submit = SubmitField('Upload File')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+        return f"File '{file.filename}' hase been uploaded"
+    return render_template('index.html', form = form)
 
 
 if(__name__ == "__main__"):
